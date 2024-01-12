71e1b0a0cfbcfb1ef9039088557fa544dea286f4
Kyrylo Ryndych <rynditch2016.serega@gmail.com>
2024-01-12 13:47:39 +0200

    create models for teams and employee

--- team/models.py
+++ team/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    team = models.ForeignKey(Team, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
